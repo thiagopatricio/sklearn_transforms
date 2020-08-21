5777b7844bfc00f3c6feaccb3beca6e9796b20f5
Thiago Patricio da Silva <30551336+thiagopatricio@users.noreply.github.com>
2020-08-20 21:50:31 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,27 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+   
+class MediaNotas(BaseException, TransformerMixin):
+    ''' Recebo uma string para ser o nome da coluna de medias'''
+    def __init__(self, nome_da_coluna):
+    self.nome = nome_da_coluna
+    def fit(self, X , y=None):
+    return self
+
+
+  def media_notas(self, x):
+    notas = []
+    notas.append(x['NOTA_DE'])
+    notas.append(x['NOTA_EM'])
+    notas.append(x['NOTA_MF'])
+    notas.append(x['NOTA_GO'])
+    media = np.sum(notas)/4
+    return pd.Series(data=[media], index=[self.nome])
+
+  def transform(self, X):
+    data = X.copy()
+    
+    coluna_media = data.apply(media_notas, axis =1)
+    data_c_media = data.join(coluna_media)
+    return data_c_media
